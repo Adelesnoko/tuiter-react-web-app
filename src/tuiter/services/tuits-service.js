73bf300e4b899a8b5960b8bc2b9c716df7528fe2
Adelesnoko <adela_feng@outlook.com>
2023-06-20 06:41:46 -0700

    updated

--- src/tuiter/services/tuits-service.js
+++ src/tuiter/services/tuits-service.js
@@ -6,7 +6,6 @@ import axios from 'axios';
 // const API_BASE = "http://localhost:4000/api";
 const API_BASE = process.env.REACT_APP_API_BASE;
 const TUITS_API = `${API_BASE}/tuits`;
-
 const request = axios.create({
     withCredentials: true,
 });
